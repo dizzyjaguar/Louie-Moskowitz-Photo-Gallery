afda882f727a3a49238f2800ede15b4e346646c6
Eli Zevin <dizzyjaguar@gmail.com>
2019-10-25 18:22:37 -0700

    Add Gallery Detail Url and home page to link to them

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("<int:pk>/", views.gallery_detail, name="gallery_detail"),
     path('about/', views.about, name='about'),
     path('gallery1/', views.gallery1, name='gallery1'),
 ]
