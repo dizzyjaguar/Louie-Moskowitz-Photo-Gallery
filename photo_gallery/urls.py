a4bf771d56c00bf698110073ac36f3589c430d29
Eli Zevin <dizzyjaguar@gmail.com>
2019-10-27 16:08:03 -0700

    Carousel is now working and grabbing the photos for the album from the models

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    #!!!!!
+    #get the path below to display the gallery title instead of primary key
     path("<int:pk>/", views.gallery_detail, name="gallery_detail"),
     path('about/', views.about, name='about'),
     path('gallery1/', views.gallery1, name='gallery1'),
