1fc5a8b4c3521e6ea8c6e69bee45f371acd85d97
Eli Zevin <dizzyjaguar@gmail.com>
2019-11-14 10:20:19 -0800

    getting ready for prod.

---
+++ louie_moskowitz/config/dev.py
@@ -0,0 +1,8 @@
+from base import *
+import logging
+
+DEBUG = True
+
+logging.basicConfig(level=logging.DEBUG)
+
+ADMINS = ['localhost']
---
+++ louie_moskowitz/config/prod.py
@@ -0,0 +1,18 @@
+from base import *
+import logging
+
+DEBUG = False
+
+logging.basicConfig(level=logging.INFO)
+
+ALLOWED_HOSTS = ['louie-gallery.herokuapp.com']
+
+DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# A comma-separated list of email addresses for admins
+ADMIN_EMAILS = os.environ.get('ADMIN_EMAILS')
+if ADMIN_EMAILS:
+    ADMINS = [admin.strip() for admin in ADMIN_EMAILS.split(',')]
